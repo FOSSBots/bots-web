8f1ea23183457fa4e783df6298e445a1fa4dea26
zppix1 <support@zppixballee.com>
2016-10-21 22:07:32 -0500

    Remove unneeded files, rename contributors.php to contribs.php
    Change-Id: I8ccc076aa93302787400847cfe19400205ecd63e

--- public_html/updateweb.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-if ($_GET['run']) {
-  # This code will run if ?run=true is set.
-  exec("/data/project/zppixbot/public_html/updatewebcode.php");
-    exec("/data/project/zppixbot/public_html/gitcommitauto.php");
-}
-?>
-<a href="?run=true">Update website and commit changes</a>
