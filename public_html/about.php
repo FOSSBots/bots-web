109b017ed830d0478919b700d617bc935f57d707
Zppix <Megadev44s.mail@gmail.com>
2016-11-10 21:19:35 +0000

    about.php addition
    Change-Id: I2b47bf23bb7389ec4990b3ec47c563b38123f322

---
+++ public_html/about.php
@@ -0,0 +1,7 @@
+// will soon contain content // 
+<html>
+  <head>
+    <title> Error! </title>
+   <body> 
+     <p> Error: This web page is not yet configured, please try again later. </p>
+     </>
