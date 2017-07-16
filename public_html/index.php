e5f19b1f84648cd02ea6be1f2589e67a576b019b
zppix1 <Megadev44s.mail@gmail.com>
2017-07-16 13:36:25 -0500

    Add additonal info to index.php
    Change-Id: Ifdc81494bb0a2bf04ab1ec7dc37891aa7fcb56f0

--- public_html/index.php
+++ public_html/index.php
@@ -1,2 +1,2 @@
-test webpage v.1
-
+For more information, click <a href="tools.wmflabs.org/zppixbot/about.php">here</a>.
+List of contributors, click <a href="tools.wmflabs.org/zppixbot/contribs.php">here</a>
