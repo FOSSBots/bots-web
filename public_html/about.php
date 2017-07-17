4f919c8209e3e96d0e8691680f13a26f3fa626f8
Zppix <Megadev44s.mail@gmail.com>
2017-07-17 16:56:22 +0000

    Update About.php with new info
    Change-Id: I7b72809007360036dc913b2770ff4cde9aac032a

--- public_html/about.php
+++ public_html/about.php
@@ -1,7 +1,6 @@
-// will soon contain content // 
 <html>
   <head>
-    <title> Error! </title>
-   <body> 
-     <p> Error: This web page is not yet configured, please try again later. </p>
+    <title> About </title>
+   <body>
+     <p> ZppixBot is an ircbot maintained by Zppix but is a expiermental version of Piaget2 and a standalone bot as well. You can watch the development as it happens here, https://phabricator.wikimedia.org/project/view/2325/</p>
      </>
