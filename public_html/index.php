64a5d8d63d4e5dff261065de6c648eaf51628683
Zppix <Megadev44s.mail@gmail.com>
2017-07-30 21:21:31 +0000

    Announce inital release of bot source code
    Change-Id: I1bc2ee9a24fadbb5ece22fb21e522a2ea06ff892

--- public_html/index.php
+++ public_html/index.php
@@ -1,2 +1,6 @@
 For more information, click <a href="/zppixbot/about.php">here</a>.
 List of contributors, click <a href="/zppixbot/contribs.php">here</a>
+
+
+<h1> Inital of ZppixBot is here! </h1>
+<p> For more information please check out, https://github.com/Pix1234/ZppixBot-Source/releases/tag/v1 !!!
