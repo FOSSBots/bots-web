d87b0b5d932c7746d4ef4d18d96041be0227468d
Reception123 <utilizator.receptie123@gmail.com>
2017-08-07 21:26:30 +0200

    Add myself to contributers for ZppixBot
    Change-Id: Ib95ca903bfb0dab1ed63b161ea69aefdc3273887

--- public_html/contribs.php
+++ public_html/contribs.php
@@ -2,3 +2,4 @@ People whom have contributed to Zppixbot
 Zppix- Operator
 tom29739- Release version bot operator, developer
 SwisterTwister- Zppixbot web design, and friend.
+Reception123 - Helps with new features
