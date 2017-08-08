3b9f9a8b06b00d4d639afc69a13c72c6079fe060
Reception123 <utilizator.receptie123@gmail.com>
2017-08-08 20:03:26 +0200

    Add links for release tag and my user page.
    Change-Id: I5990650f4195e0cfe5473257ace40c03f7af1f9f

--- public_html/index.php
+++ public_html/index.php
@@ -1,8 +1,8 @@
-For more information, click <a href="/zppixbot/about.php">here</a>.
-List of contributors, click <a href="/zppixbot/contribs.php">here</a>
+For more information, click <a href="/zppixbot/about.php">here</a>. </br>
+List of contributors, click <a href="/zppixbot/contribs.php">here</a> </br>
 
+If you need ZppixBot in your channel, please contact Zppix. </br>
 
 <h1> Inital release of ZppixBot is here! </h1>
-<p> For more information please check out, https://github.com/Pix1234/ZppixBot-Source/releases/tag/v1 !!!
+<p> For more information please check out <a href="https://github.com/Pix1234/ZppixBot-Source/releases/tag/v1">the release tag</a> !!!</br>
 
-If you need ZppixBot in your channel, please contact Zppix.
