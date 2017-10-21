965b1f5cf900437ea9fa638113b861b952830233
MacFan4000 <paulfkeffer@gmail.com>
2017-10-21 19:08:32 +0000

    Fix links per https://github.com/wikimedia/labs-tools-ZppixBot/pull/1/
    Change-Id: I50c00f3decf2e19e1756bdf46c71b08c91e30cd1

--- public_html/contribs.php
+++ public_html/contribs.php
@@ -2,4 +2,4 @@ People whom have contributed to Zppixbot<br />
 Zppix- Operator<br />
 tom29739- Release version bot operator, developer<br />
 SwisterTwister- Zppixbot web design, and friend.<br />
-<a href=https://meta.miraheze.org/wiki/User:Reception123">Reception123</a> - Helps with new features
+<a href="https://meta.miraheze.org/wiki/User:Reception123">Reception123</a> - Helps with new features
--- public_html/index.php
+++ public_html/index.php
@@ -1,10 +1,10 @@
 For more information, click <a href="/zppixbot/about.php">here</a>. </br>
 List of contributors, click <a href="/zppixbot/contribs.php">here</a> </br>
 
-If you need ZppixBot in your channel, please contact Zppix or <a href=https://meta.miraheze.org/wiki/User:Reception123">Reception123</a> </br>
+If you need ZppixBot in your channel, please contact Zppix or <a href="https://meta.miraheze.org/wiki/User:Reception123">Reception123</a> </br>
 
 <h1> The second release of ZppixBot is now here! </h1>
 <p> For more information please check out <a href="https://github.com/Pix1234/ZppixBot-Source/releases/tag/v2">the release tag</a> !!!</br>
 
 <b>Previous releases</b>:</br>
-<a href"https://github.com/Pix1234/ZppixBot-Source/releases/tag/v1">v1 (30 July 2017)</a></br>
+<a href="https://github.com/Pix1234/ZppixBot-Source/releases/tag/v1">v1 (30 July 2017)</a></br>
