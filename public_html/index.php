db29f17eeac4832dbf1ce984eb75278c4ef35987
MacFan4000 <paulfkeffer@gmail.com>
2017-12-13 01:22:34 +0000

    Update how to get the Bot in your channel
    Change-Id: Ie27d696c177bb10163b6cff6b6739a75a947a473

--- public_html/index.php
+++ public_html/index.php
@@ -2,7 +2,7 @@ For more information, click <a href="/zppixbot/about.php">here</a>. </br>
 For a list of commands, click <a href="/zppixbot/documentation.html">here</a>. </br>
 List of contributors, click <a href="/zppixbot/contribs.php">here</a> </br>
 
-If you need ZppixBot in your channel, please contact Zppix or <a href="https://meta.miraheze.org/wiki/User:Reception123">Reception123</a> </br>
+If you would like ZppixBot in your channel, join <a href="irc://chat.freenode.net/#ZppixBot">#ZppixBot</a> and bug Zppix or Reception123. </br>
 
 <h1> The second release of ZppixBot is now here! </h1>
 <p> For more information please check out <a href="https://github.com/Pix1234/ZppixBot-Source/releases/tag/v2">the release tag</a> !!!</br>
