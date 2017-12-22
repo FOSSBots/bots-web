15cb7e3d20fb9e370109e05cf594eb5498322579
MacFan4000 <paulfkeffer@gmail.com>
2017-12-22 20:18:18 +0000

    Update
    Change-Id: I2fe14008e204f02a3e472b6f6246b74ffadcf8ab

--- public_html/contribs.php
+++ public_html/contribs.php
@@ -3,6 +3,6 @@
 Zppix- Operator<br />
 tom29739- Release version bot operator, developer<br />
 SwisterTwister- Zppixbot web design, and friend.<br />
-<a href="https://meta.miraheze.org/wiki/User:Reception123">Reception123</a> - Helps with new features<br/>
-MacFan4000 -Web design<br/>
+<a href="https://meta.miraheze.org/wiki/User:Reception123">Reception123</a> - Helps with new features/Operator<br/>
+MacFan4000 -Web design/Operator<br/>
 Phantom42 -Web design/developer
