983b697021f05fb2a56bf295d7f70953f33dd1f7
Reception123 <utilizator.receptie123@gmail.com>
2017-12-22 20:30:15 +0000

    Missing space
    Change-Id: I31a0884f4fe02f64bd0a85cca7b4e6bebecc9dab

--- public_html/about.php
+++ public_html/about.php
@@ -1,4 +1,4 @@
 <title>About -ZppixBot</title>
 <h1>About</h1>
 <body>
-ZppixBot is an IRC bot running <a href="https://sopel.chat">Sopel</a> 6.5.1 and Python 2.7.9. The current release of ZppixBot customizations is version 2.0. ZppixBot has lots of commands and more can be added by writing more modules. The home channel for ZppixBot is <a href="irc://chat.freenode.net/#ZppixBot">#ZppixBot</a>where you can get help from the maintainers. ZppixBot's current maintainers are Zppix and Reception123.</body>
+ZppixBot is an IRC bot running <a href="https://sopel.chat">Sopel</a> 6.5.1 and Python 2.7.9. The current release of ZppixBot customizations is version 2.0. ZppixBot has lots of commands and more can be added by writing more modules. The home channel for ZppixBot is <a href="irc://chat.freenode.net/#ZppixBot">#ZppixBot</a> where you can get help from the maintainers. ZppixBot's current maintainers are Zppix and Reception123.</body>
