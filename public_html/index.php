062fb33a87352f99ce5f86f7bb69b635cb5aa108
Reception123 <utilizator.receptie123@gmail.com>
2017-12-24 17:43:14 +0000

    Correct typo
    Change-Id: Iebf3bbfe8576fea53258ee621fed30dda40103b7

--- public_html/index.php
+++ public_html/index.php
@@ -3,7 +3,7 @@ For more information, click <a href="/zppixbot/about.php">here</a>. </br>
 For a list of commands, click <a href="/zppixbot/documentation.html">here</a>. </br>
 List of contributors, click <a href="/zppixbot/contribs.php">here</a> </br>
 
-If you would like ZppixBot in your channel, join <a href="irc://chat.freenode.net/#ZppixBot">#ZppixBot</a> and say .addchannnel </br>
+If you would like ZppixBot in your channel, join <a href="irc://chat.freenode.net/#ZppixBot">#ZppixBot</a> and say .addchannel </br>
 
 <h1> The third release of ZppixBot is now here! </h1>
 <p> For more information please check out <a href="https://github.com/Pix1234/ZppixBot-Source/releases/tag/v3">the release tag</a> !!!</br>
