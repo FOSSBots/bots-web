aabe6c74e6afa2462fcb48205d1d5f085832a5ea
Reception123 <utilizator.receptie123@gmail.com>
2017-12-24 17:40:52 +0000

    change how to request ZppixBot in channel
    Change-Id: I7726c003212a1efb1523d8df5abb2a340eda6740

--- public_html/index.php
+++ public_html/index.php
@@ -1,9 +1,9 @@
-<title>Home -ZppixBot</title>
+<title>Home - ZppixBot</title>
 For more information, click <a href="/zppixbot/about.php">here</a>. </br>
 For a list of commands, click <a href="/zppixbot/documentation.html">here</a>. </br>
 List of contributors, click <a href="/zppixbot/contribs.php">here</a> </br>
 
-If you would like ZppixBot in your channel, join <a href="irc://chat.freenode.net/#ZppixBot">#ZppixBot</a> and bug MacFan4000, Zppix or Reception123. </br>
+If you would like ZppixBot in your channel, join <a href="irc://chat.freenode.net/#ZppixBot">#ZppixBot</a> and say .addchannnel </br>
 
 <h1> The third release of ZppixBot is now here! </h1>
 <p> For more information please check out <a href="https://github.com/Pix1234/ZppixBot-Source/releases/tag/v3">the release tag</a> !!!</br>
