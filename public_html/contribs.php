5646e042a221adb3a53abef2edc6fc9e73efb14d
MacFan4000 <paulfkeffer@gmail.com>
2018-01-20 14:12:42 -0500

    Add navbar and update htnl/css
    Change-Id: Ifb95c846ed9e0da33fb7d638ee56de98a718f74c

--- public_html/contribs.php
+++
@@ -1,8 +0,0 @@
-<title>Contributors -ZppixBot</title>
-<h1>People whom have contributed to Zppixbot</h1><br />
-Zppix- Operator<br />
-tom29739- Release version bot operator, developer<br />
-SwisterTwister- Zppixbot web design, and friend.<br />
-<a href="https://meta.miraheze.org/wiki/User:Reception123">Reception123</a> - Helps with new features/Operator<br/>
-MacFan4000 -Web design/Operator<br/>
-Phantom42 -Web design/developer
